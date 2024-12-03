47baea0a68dc2ceb886a9acef326e669218fcf64
João Pedro <jpbcs2005@gmail.com>
2024-12-02 23:30:10 -0300

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Space88/', // Altere para o nome do seu repositório
   server: {
     port: 3001, // Altere para a porta desejada
   },
